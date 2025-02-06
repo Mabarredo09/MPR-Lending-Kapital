e6852fb9520b0f338f83049821b16a13b0a210ff
Mabarredo09 <mbarredo2n.neust@gmail.com>
2025-02-06 11:17:45 +0800

    Added profile

---
+++ profile.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS -->
+    <link rel="stylesheet" href="styles/profile.css">
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Source+Serif+4:ital,opsz,wght@0,8..60,200..900;1,8..60,200..900&display=swap"
+        rel="stylesheet">
+    <!-- SweetAlert2 -->
+    <script src="node_modules/sweetalert2/dist/sweetalert2.all.min.js"></script>
+    <!-- Zooming -->
+    <script src="node_modules/zooming/build/zooming.min.js"></script>
+
+    <title>Profile</title>
+</head>
+<body>
+<nav>
+        <div class="dashboard-container">
+
+            <div class="dashboard-header">
+                <div class="logo-container">
+                    <img src="images/dashboard.png" alt="Dashboard Logo">
+                </div>
+                <div class="dashboard-header-right">
+                    <div class="notification">
+                    <div class="notification-icon">
+                        <svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                            <path fill="currentColor"
+                                d="M12 22c1.1 0 2-.9 2-2h-4a2 2 0 0 0 2 2m6-6v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-2 2v1h16v-1z" />
+                        </svg>
+                        <p>Notification</p>
+                    </div>
+                    <div class="notification-dropdown">
+                        <div class="notification-dropdown-content">
+                            <p>No new notifications</p>
+                            <a href="#">View all notifications</a>
+                        </div>
+                            
+                    </div>
+                </div>
+                    <div class="profile">
+                        <img src="images/profile.jpg" alt="Profile picture of John Doe">
+                        <div class="profile-dropdown">
+                        <p>Welcome, John Doe</p>
+                        <div class="profile-dropdown-content">
+                            <a href="#">Profile</a>
+                            <a href="#">Manage User Access</a>
+                            <a href="#">Audit Logs</a>
+                            <a href="#">Logout</a>
+                        </div>
+                    </div>
+                    <svg class="arrow-down-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                            <path fill="currentColor" d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6l-6-6z" />
+                        </svg>
+                </div>
+            </div>
+    </nav>
+    <div class="profile-container">
+        <h1>Profile</h1>
+        <div class="profile-content">
+
+            <div class="profile-picture">
+                <h1>Profile Picture</h1>
+                <form action="" method="post">
+                    <div class="upload-profile-picture-container">
+                    <img src="images/profile.jpg" alt="Profile picture of John Doe" id="profile-picture-preview">
+                    <div class="upload-profile-picture-btn-container">
+                    <p>Upload a new profile picture</p>
+                    <input type="file" name="profile-picture" id="profile-picture-upload" accept="image/*">
+                    </div>
+                    </div>
+                    <div class="confirm-upload-profile-btn-container">
+                    <button id="cancel-upload-profile-btn">Cancel</button>
+                    <button type="submit" id="upload-profile-btn" disabled>Confirm</button>
+                    </div>
+                </form>
+            </div>
+
+           <div class="profile-info">
+                <h1>Personal Information</h1>
+                <form action="#" method="post">
+                    <label for="Name">Full Name</label><br>
+                    <input type="text" id="Name" class="inputs" name="Name" value="John Doe" disabled><br>
+                    <label for="Email">Email</label><br>
+                    <input type="email" id="Email" class="inputs" name="Email" value="jdoe@up.edu.ph" disabled><br>
+                    <label for="Password">Password</label><br>
+                    <input type="password" id="Password" class="inputs" name="Password" value="********" disabled><br>
+                    <label for="Confirm Password">Confirm Password</label><br>
+                    <input type="password" id="Confirm Password" class="inputs" name="Confirm Password" disabled ><br>
+                </form>
+
+                <div class="profile-button-container">
+                    <button id="edit-profile-btn">Edit Profile</button>
+                    <button id="save-profile-btn">Save Profile</button>
+                </div>
+           </div>
+        </div>
+    </div>
+</body>
+<script src="scripts/profile.js"></script>
+</html>
---
+++ scripts/profile.js
@@ -0,0 +1,101 @@
+let profileDropdown = document.querySelector('.profile-dropdown');
+let arrowDownIcon = document.querySelector('.arrow-down-icon'); 
+let editProfileButton = document.getElementById('edit-profile-btn');
+let saveProfileButton = document.getElementById('save-profile-btn');
+let inputs = document.querySelectorAll('.inputs');
+const zooming = new Zooming();
+const notificationIcon = document.querySelector('.notification-icon');
+const notificationDropdown = document.querySelector('.notification-dropdown');
+
+let profilePicturePreview = document.getElementById('profile-picture-preview');
+let profilePictureUpload = document.getElementById('profile-picture-upload');
+let uploadConfirmBtn = document.getElementById('upload-profile-btn');
+let uploadCancelBtn = document.getElementById('cancel-upload-profile-btn');
+
+profileDropdown.addEventListener('mouseover', function(){
+    console.log('hovered');
+    arrowDownIcon.style.transform = 'rotate(180deg)';
+    arrowDownIcon.style.transition = 'transform 0.5s ease';
+});
+
+profileDropdown.addEventListener('mouseout', function(){
+    arrowDownIcon.style.transform = 'rotate(0deg)';
+    arrowDownIcon.style.transition = 'transform 0.5s ease';
+});
+
+notificationIcon.addEventListener('click', function() {
+    notificationDropdown.style.display = notificationDropdown.style.display === 'block' ? 'none' : 'block';
+});
+// Close the dropdown if the user clicks outside of it
+window.addEventListener('click', function(event) {
+    if (!event.target.closest('.notification')) {
+        if (notificationDropdown.style.display === 'block') {
+            notificationDropdown.style.display = 'none';
+        }
+    }
+});
+
+// Zooming
+zooming.listen('#profile-picture-preview');
+// Profile Picture Preview
+profilePictureUpload.addEventListener('change', function() {
+    const file = profilePictureUpload.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            profilePicturePreview.src = e.target.result;
+        };
+        reader.readAsDataURL(file);
+        zooming.listen('#profile-picture-preview');
+    }
+
+    uploadConfirmBtn.disabled = false;
+    uploadConfirmBtn.style.backgroundColor = '#1E3E62';
+    uploadCancelBtn.style.display = 'block';
+});
+
+uploadCancelBtn.addEventListener('click', function() {
+    profilePicturePreview.src = '';
+    uploadConfirmBtn.disabled = true;
+    uploadConfirmBtn.style.backgroundColor = '#c1c1c1';
+    uploadCancelBtn.style.display = 'none';
+});
+
+
+// Edit Profile Information
+editProfileButton.addEventListener('click',function(){
+    console.log('clicked');
+    editProfileButton.innerHTML = editProfileButton.innerHTML === 'Edit Profile' ? 'Cancel' : 'Edit Profile';
+    if (editProfileButton.innerHTML === 'Cancel') {
+        editProfileButton.style.backgroundColor = '#FF0000';
+    } else {
+        editProfileButton.style.backgroundColor = '#1E3E62';
+    }
+
+    if (editProfileButton.innerHTML === 'Cancel') {
+        saveProfileButton.disabled = false;
+    } else {
+        saveProfileButton.disabled = true;
+    }
+
+    if (editProfileButton.innerHTML === 'Cancel') {
+        saveProfileButton.style.backgroundColor = '#1E3E62';
+    } else {
+        saveProfileButton.style.backgroundColor = '#c1c1c1';
+    }
+
+    if (editProfileButton.innerHTML === 'Cancel') {
+        inputs.forEach(input => input.disabled = false);
+    } else {
+        inputs.forEach(input => input.disabled = true);
+    }
+});
+
+saveProfileButton.addEventListener('click',function(){
+    console.log('clicked');
+    saveProfileButton.disabled = true;
+    saveProfileButton.style.backgroundColor = '#c1c1c1';
+    editProfileButton.innerHTML = 'Edit Profile';
+    editProfileButton.style.backgroundColor = '#1E3E62';
+    inputs.forEach(input => input.disabled = true);
+});
